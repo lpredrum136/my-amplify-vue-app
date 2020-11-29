b87c973e1738a0ee3e65cdfaa4e890f04d025dcb
Henry Nguyen <nguyenhai1591@gmail.com>
2020-11-29 19:41:55 +1000

    Add lots of basic functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,106 @@
 <template>
   <div id="app">
-    <amplify-authenticator>
+    <!-- <amplify-authenticator>
       <div>
         <h1>Hey, {{ user.username }}!</h1>
         <amplify-sign-out></amplify-sign-out>
       </div>
-    </amplify-authenticator>
+    </amplify-authenticator> -->
+
+    <!--SIGN IN-->
+    <form @submit="onSubmit">
+      <input type="text" name="username" id="username" v-model="username" />
+      <input type="password" name="password" id="password" v-model="password" />
+      <input type="submit" value="Login" />
+    </form>
+    <button @click="logout">Sign out</button>
+
+    <!--GET DATA VIA API-->
+    <button @click="getCustomers">Get all customers</button>
   </div>
 </template>
 
 <script>
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-// import { Auth } from 'aws-amplify';
+// import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { Auth, API } from 'aws-amplify';
 
 export default {
   name: 'App',
   data() {
     return {
-      user: {}
+      user: {},
+      username: '',
+      password: ''
     };
   },
   created() {
     // authentication state managament
-    onAuthUIStateChange((state, user) => {
+    /* onAuthUIStateChange((state, user) => {
       // set current user and load data after login
       if (state === AuthState.SignedIn) {
         this.user = user;
       }
+    }); */
+    this.checkAuth();
+  },
 
-      /* Auth.currentAuthenticatedUser().then(res => {
-        console.log(res);
-      }); */
-    });
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      // console.log(this.username, this.password);
+      this.login();
+    },
+
+    async login() {
+      try {
+        const user = await Auth.signIn({
+          username: this.username,
+          password: this.password
+        });
+        console.log(user);
+      } catch (error) {
+        console.log('error signing in: ', error);
+      }
+    },
+    async logout() {
+      try {
+        await Auth.signOut();
+        console.log('Sign out successfully');
+      } catch (error) {
+        console.log('error signing out: ', error);
+      }
+    },
+    async checkAuth() {
+      console.log('checking auth');
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        console.log('Authenticated user: ', user);
+      } catch (error) {
+        console.log('User not logged in', error);
+      }
+    },
+    async getCustomers() {
+      const apiName = 'customerAPI';
+      const path = '/customers';
+      const init = {};
+      // optional
+      /* const init = {
+        headers: {
+
+        },
+        response: true,
+        queryStringParameters: {
+          name: 'param'
+        }
+      } */
+
+      try {
+        const res = await API.get(apiName, path, init);
+        console.log(res.success);
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
   }
 };
 </script>
